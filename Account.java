c9fcca6ab00ed9d110a97aa4bab3e25308a1eed0
SlavaKudravcev <123372629+SlavaKudravcev@users.noreply.github.com>
2023-12-28 12:50:05 +0200

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,27 @@
+public class Account {
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited $" + amount + " to account " + accountNumber);
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn $" + amount + " from account " + accountNumber);
+        } else {
+            System.out.println("Insufficient funds in account " + accountNumber);
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
